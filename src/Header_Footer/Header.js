b8a816159e5cd5ddbea41f817b652fbac5be2dc7
Brandon <brandonbgrayson@gmail.com>
2022-05-17 10:10:00 -0400

    added background img with navbar on top

--- src/Header_Footer/Header.js
+++ src/Header_Footer/Header.js
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import MonitorHeartIcon from '@mui/icons-material/MonitorHeart';
-import { Divider } from '@mui/material';
+import { Divider, Box } from '@mui/material';
 
 export default function Navbar() {
   return (
-    <>
+    <Box sx={{ position: 'absolute', top: '8px', left: '16px' }}>
       <MonitorHeartIcon color='primary' fontSize="large" className='nav-icon' sx={{ marginLeft: '10vw' }} />
       <Divider />
-    </>
+    </Box>
 
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -7,6 +7,7 @@ export default function Home() {
         <Grid container >
             <Grid item xs={12} sx={{ height: "98vh" }} width="100vw" >
                 <img src="./img/Pulse-background.jpeg" style={{ height: '100%', width: '100%' }} />
+                <Navbar />
             </Grid>
         </Grid>
     )
