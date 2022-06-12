fff8a4aa56e7f898ade7c9c092cf7d7cc48749e3
Brandon <brandonbgrayson@gmail.com>
2022-06-12 10:24:18 -0400

    bar section sized for mobile

--- src/Home.js
+++ src/Home.js
@@ -41,7 +41,7 @@ export default function Home() {
             </Grid>
             <Grid container mt={5}>
                 <Grid item xs={12} display='flex' flexDirection="row">
-                    <Box sx={{ width: '4px', backgroundColor: '#24307D', height: '104px', marginLeft: '5vw', marginRight: '5vw', }}></Box>
+                    <Box className="bar"></Box>
                     <Typography id="about" sx={{ marginRight: '5vw' }}>
                         Pulse Media is a branding agency based in Mt Laurel, NJ.
                         We help home remodeling companies generate referrals by promoting their brand online.
